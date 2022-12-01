173c3217a272be4268360eb8f45cd7df0cfb457d
JoshNarindra <jbhawanlall@gmail.com>
2022-12-01 12:05:14 +0000

    Added Reset, Exit Buttons.

--- src/main/java/com/example/javafxlogin/LoginApplication.java
+++ src/main/java/com/example/javafxlogin/LoginApplication.java
@@ -11,7 +11,7 @@ public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 350, 400);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/javafxlogin/LoginController.java
+++ src/main/java/com/example/javafxlogin/LoginController.java
@@ -1,15 +1,22 @@
 package com.example.javafxlogin;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import java.util.HashMap;
+import javafx.application.Platform;
+//import java.util.HashMap;
+//import javafx.event.ActionEvent;
 
 public class LoginController {
     public Label login;
     public Button sign_in;
+
+    public Button exit;
+
+    public Button reset;
     @FXML
     private Label Text;
 
@@ -22,17 +29,51 @@ public class LoginController {
     @FXML
     protected void onSignInButtonClick() {
 
+//      Functionality for one username-input combo - NOT CASE SENSITIVE.
         String usernames = "user";
         String passwords = "password";
 
         if (usernames.equals(username.getText()) && passwords.equals(password.getText())) {
             Text.setText("Login Successful.");
-        }
-        else if (username.getText().isEmpty() || password.getText().isEmpty()){
+        } else if (username.getText().isEmpty() || password.getText().isEmpty()) {
             Text.setText("At least one field is empty please try again.");
-        }
-        else {
+        } else {
             Text.setText("Username or Password incorrect.");
         }
+
+//        //Hashmap to store username-password combinations
+//        HashMap<String, String> loginPair = new HashMap<String, String>();
+//
+//        loginPair.put("user","password");
+//        loginPair.put("user1","password1");
+//        loginPair.put("user2","password2");
+//
+//        boolean check = false;
+//
+//        for (String i : loginPair.keySet()) {
+//            if (i.equals(username.getText()) && loginPair.get(i).equals(password.getText())){
+//                Text.setText("Login Successful.");
+//            }
+//            else {
+//                Text.setText("Login Unsuccessful.");
+//            }
+//        }
     }
+
+
+    @FXML
+    public void onExitButtonClick() {
+            Platform.exit();
+    }
+
+    @FXML
+    public void onResetButtonClick(){
+        textEmpty();
+    }
+
+    public void textEmpty(){
+        username.setText("");
+        password.setText("");
+    }
+
 }
