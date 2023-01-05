a96dd1396df4b69750d54acccc9eb1e8400bb312
Josh <jbhawanlall@gmail.com>
2023-01-05 14:12:28 +0000

    Fixed functionality of both controllers + bugs

--- src/main/java/com/example/javafxlogin/RegisterController.java
+++ src/main/java/com/example/javafxlogin/RegisterController.java
@@ -7,9 +7,9 @@ import javafx.event.ActionEvent;
 
 public class RegisterController
 {
-    @FXML
-    public void initialize() {
-    }
+    //@FXML
+    //public void initialize() {
+    //}
 
     @FXML
     public TextField usernametextfield;
@@ -32,8 +32,44 @@ public class RegisterController
     private void registerUser() {
         DatabaseConnection x = new DatabaseConnection();
 
-        String query = "insert into UserDetails(username,userpassword) values ('" + usernametextfield.getText() + "'" + ", '" + passwordtextfield.getText() + "');";
+        String query = "select COUNT(1) from UserDetails where username= '" + usernametextfield.getText() + "'";
 
+        try {
+
+            var stmt = x.getConnection().prepareStatement(query);
+            var rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                //if statement checks if username inputted already exists in database. If it does not it registers the user into the system.
+                if (rs.getInt(1) == 1) {
+                    System.out.println("Username Already Exists, Please Try Again");
+                    textEmpty();
+                } else {
+                    System.out.println("Username Does Not Exist, user added to database");
+                    addUserToDatabase();
+                }
+            }
+
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void addUserToDatabase() {
+        DatabaseConnection x = new DatabaseConnection();
+
+        String query = "insert into UserDetails(username,userpassword) values ('" + usernametextfield.getText() + "'" + ", '" + passwordtextfield.getText() + "');";
+        try {
+
+            var stmt = x.getConnection().prepareStatement(query);
+            stmt.executeUpdate();
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
     }
 
     public void textEmpty() {
